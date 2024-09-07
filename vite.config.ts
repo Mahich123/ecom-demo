2a2ba3f5040682f8ee083ced520ed0e76c75a034
Mahi <mahichowdhury587@gmail.com>
2024-09-07 12:02:16 +0600

    resolved utis error

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import path from "path"
+
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import * as path from 'path';
 
 export default defineConfig({
   plugins: [react()],
